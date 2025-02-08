73ca437b9e6280b53c6068cf43b8af4c0bbdc878
Sofia Pham <sofiapham98@gmail.com>
2025-02-08 03:42:43 -0500

    adding google analytics

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,20 @@ import Navbar from "./components/Navbar";
 
 import { Home, About, Projects, Contact } from "./pages";
 
+import ReactGA from "react-ga4";
+import React, { useEffect } from "react";
+
 const App = () => {
+  const MEASUREMENT_ID = import.meta.env.GOOGLE_ANALYTICS_MEASUREMENT_ID;
+  ReactGA.initialize(`${MEASUREMENT_ID}`);
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: window.location.pathname,
+      title: document.title,
+    });
+  }, []);
+
   return (
     <main className="bg-slate-300/20 h-full min-h-screen">
       <Router>
